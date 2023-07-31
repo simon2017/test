808cf2f577b3ad0af5659e04302b88c98b41b7b3
Simon <sgutierc@emeal.nttdata.com>
2023-07-31 16:01:53 -0400

    Test Funcionales

--- ApiRestfull/src/test/java/com/nttdata/UserControllerTests.java
+++ ApiRestfull/src/test/java/com/nttdata/UserControllerTests.java
@@ -44,9 +44,31 @@ public class UserControllerTests {
 
 	/**
 	 * Genera usuario dummy
+	 * 
+	 * @return
+	 */
+	private UserData getDummyUser() {
+		return getDummyUser("test", "test@test.com", "Tttest12");
+	}
+
+	/**
+	 * Genera usuario dummy
+	 * 
 	 * @return
 	 */
 	private UserData getDummyUser(String name, String email) {
+		return getDummyUser(name, email, "Tttest12");
+	}
+
+	/**
+	 * Genera usuario dummy
+	 * 
+	 * @param name
+	 * @param email
+	 * @param password
+	 * @return
+	 */
+	private UserData getDummyUser(String name, String email, String password) {
 		Phone phone = new Phone();
 		phone.setNumber("999999999");
 		phone.setCitycode(45);
@@ -56,7 +78,7 @@ public class UserControllerTests {
 
 		data.setEmail(email);
 		data.setName(name);
-		data.setPassword("Sssimon12");
+		data.setPassword(password);
 		data.setPhones(Arrays.asList(phone));
 
 		return data;
@@ -64,20 +86,34 @@ public class UserControllerTests {
 
 	@Test
 	void registerUser_assertOK() {
-		RegisterResponse response = controller.register(getDummyUser("test_OK","ssOK@sss.com"));
+		RegisterResponse response = controller.register(getDummyUser());
 		assertThat(response.getCode()).isEqualTo(HttpStatus.OK.value() + "");
 	}
 
 	@Test
-	void registerUser_assertKO() {
-		UserData userA=getDummyUser("test_KO_A","ssKO@sss.com");
-		UserData userB=getDummyUser("test_KO_B","ssKO@sss.com");
+	void registerUser_duplicatedEmail() {
+		UserData userA = getDummyUser("test_KO_A", "ssKO@sss.com");
+		UserData userB = getDummyUser("test_KO_B", "ssKO@sss.com");
 		userB.setName(userA.getName() + "_B");
-		
-		//Primero guardamos data de usuario A, se espera OK
-		assertThat(controller.register(userA).getCode()).isEqualTo(HttpStatus.OK.value() + "");	
-		//Luego guardamos data de usuario B, se espera KO por correo duplicado
+
+		// Primero guardamos data de usuario A, se espera OK
+		assertThat(controller.register(userA).getCode()).isEqualTo(HttpStatus.OK.value() + "");
+		// Luego guardamos data de usuario B, se espera KO por correo duplicado
 		assertThat(controller.register(userB).getCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value() + "");
+	}
+
+	@Test
+	void registerUser_invalidPassword() {
+		UserData user = getDummyUser("test_pswd", "ss_email@sss.com","password");
+		//Esperamos KO por formato de password invalido
+		assertThat(controller.register(user).getCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value() + "");
+	}
+	
+	@Test
+	void registerUser_invalidEmail() {
+		UserData user = getDummyUser("test_email", "ss_emailsss","ss@sss.com");
+		//Esperamos KO por formato de correo invalido
+		assertThat(controller.register(user).getCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value() + "");
 	}
 
 }
